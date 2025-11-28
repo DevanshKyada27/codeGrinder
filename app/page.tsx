10d621fc12d12472a04721e0839270295f0e02e0
DevanshKyada27 <devansh.kyada27@gmail.com>
2025-11-28 19:52:31 +0530

    Made Landing Screen of the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Header from "./_components/Header";
+import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
-   <div>
-      <Button>Click</Button>
+   <div className="flex flex-col items-center">
+      {/* Navbar  */}
+      <Header />
+
+      {/* Hero section  */}
+      <Hero />
    </div>
    
   );
